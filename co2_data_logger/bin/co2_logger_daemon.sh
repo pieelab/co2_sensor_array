6ac9aa70515d5fbec2300e06538747711eb056fd
Quentin Geissmann <qgeissmann@gmail.com>
2019-10-30 00:28:08 -0700

    voila

--- co2_data_logger/bin/co2_logger_daemon.sh
+++ co2_data_logger/bin/co2_logger_daemon.sh
@@ -20,6 +20,20 @@ WantedBy=multi-user.target" > /etc/systemd/system/co2_logger.service
     systemctl restart co2_logger.service
     echo "restarting services"
 else
+
+
+# Make sure that NOBODY can access the server without a password
+#mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+
+    mysql -e "DROP USER  IF EXISTS ''@'localhost';"
+    mysql -e "DROP USER  IF EXISTS ''@'$(hostname)';"
+    mysql -e "DROP DATABASE IF EXISTS test;"
+    mysql -e "FLUSH PRIVILEGES;"
+    mysql -e "CREATE DATABASE IF NOT EXISTS co2_sensors;"
+    mysql -e "CREATE USER IF NOT EXISTS 'co2_logger'@'localhost' IDENTIFIED BY 'co2_logger';"
+    mysql -e "GRANT ALL PRIVILEGES ON co2_sensors.* TO 'co2_logger'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
+
     co2_logger_daemon.py
 fi
 
--- make_image.sh
+++ make_image.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 GIT_REPO=https://github.com/qgeissmann/co2_sensor_array
-#ZIP_IMG=2019-09-26-raspbian-buster-lite.zip
-#RASPBIAN_URL=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-09-30/$ZIP_IMG
+ZIP_IMG=2019-09-26-raspbian-buster-lite.zip
+RASPBIAN_URL=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-09-30/$ZIP_IMG
 
 
 ZIP_IMG=2019-07-10-raspbian-buster-lite.zip
@@ -11,6 +11,7 @@ RASPBIAN_URL=http://director.downloads.raspberrypi.org/raspbian_lite/images/rasp
 
 OS_IMG_NAME=$(date "+%Y-%m-%d")_os_image.img
 MOUNT_DIR=/mnt/pitally_root
+MOUNT_DIR_LARGE=/mnt/large_pitally_root
 
 
 # if not in chroot
@@ -20,12 +21,28 @@ then
     wget $RASPBIAN_URL -nc
     unzip -o $ZIP_IMG
     mv *raspbian*.img $OS_IMG_NAME
-    IMG_FILE=$(ls *.img)
     DEV="$(losetup --show -f -P "$OS_IMG_NAME")"
+    LARGE_OS_IMG=large_$OS_IMG_NAME
+    dd bs=1M count=5200 if=/dev/zero of=$LARGE_OS_IMG
+    DEV_LARGE="$(losetup --show -f -P "$LARGE_OS_IMG")"
+    dd if=$DEV of=$DEV_LARGE bs=64K conv=noerror,sync status=progress
+    losetup -d $DEV
 
+fdisk $DEV_LARGE << EOF
+d
+2
+n
+p
+FIXME
+
+
+w
+EOF
+    e2fsck ${DEV_LARGE}p2
+    resize2fs ${DEV_LARGE}p2
     mkdir -p $MOUNT_DIR
-    mount ${DEV}p2 $MOUNT_DIR
-    mount ${DEV}p1 $MOUNT_DIR/boot
+    mount ${DEV_LARGE}p2 $MOUNT_DIR
+    mount ${DEV_LARGE}p1 $MOUNT_DIR/boot
 
     cp $(which qemu-arm-static) ${MOUNT_DIR}/usr/bin
     cp make_image.sh ${MOUNT_DIR}/root/
@@ -34,33 +51,29 @@ then
     set +e
     systemd-nspawn  --directory ${MOUNT_DIR} /root/make_image.sh
 
-    umount ${DEV}p1
-    umount ${DEV}p2
-    losetup -d $DEV
+    umount ${DEV_LARGE}p1
+    umount ${DEV_LARGE}p2
+    losetup -d $DEV_LARGE
 
 else
+    mv /etc/ld.so.preload /etc/bak-ld.so.preload
     touch /boot/ssh
     apt-get update
     apt-get upgrade --assume-yes
-    #fixme hack around small image size
-    apt-get clean
-    apt-get install tree ipython3 libatlas-base-dev tcpdump nmap mariadb-server  python3-pip iputils-ping  git lftp npm --assume-yes
-    #fixme hack around small image size
-    apt-get clean
-
+    apt-get install tree ipython3 libatlas-base-dev tcpdump nmap python3-pip iputils-ping  git lftp npm  mariadb-server --assume-yes
     SKIP_WARNING=1 rpi-update
 
     pip3 install --upgrade pip
     apt-get remove python3-pip --assume-yes
 
     ## the camera and network are enabled when the machine boots for the first time
-    mysql_secure_installation # root -> root
+#    mysql_secure_installation # root -> root
     systemctl enable mariadb
-    systemctl start mariadb
-    mysql -e "CREATE USER 'co2_logger'@'localhost' IDENTIFIED BY 'co2_logger';"
-    mysql -e "CREATE DATABASE co2_sensors";
-    mysql -e "GRANT ALL PRIVILEGES ON co2_sensors.* TO 'co2_logger'@'localhost'";
-    mysql -e "FLUSH PRIVILEGES";
+#    systemctl start mariadb
+#    mysql -e "CREATE USER 'co2_logger'@'localhost' IDENTIFIED BY 'co2_logger';"
+#    mysql -e "CREATE DATABASE co2_sensors";
+#    mysql -e "GRANT ALL PRIVILEGES ON co2_sensors.* TO 'co2_logger'@'localhost'";
+#    mysql -e "FLUSH PRIVILEGES";
 
     ###
     ## stack
@@ -73,6 +86,9 @@ else
     nano /etc/co2_data_logger.conf
 
     co2_logger_daemon.sh --enable-service
+    systemctl disable  wpa_supplicant
+    systemctl enable  wpa_supplicant@wlan0.service
+
 #    rm -rf pitally
     exit
 
@@ -85,24 +101,32 @@ else
 
 fi
 
-#to clone: dd if=/dev/mmcblk0  bs=32M | gzip -c  > ./2019-10-29_pi_clone.img.gz
-# to restore: gunzip -c ./2019-10-29_pi_clone.img.gz | dd of=/dev/mmcblk0
+#eduroam
 
 
 
-# ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-# update_config=1
-# country=CA
+
+nano /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=CA
+
+network={
+   ssid="eduroam"
+   scan_ssid=1
+   key_mgmt=WPA-EAP
+   eap=PEAP
+   identity=""
+   password=""
+   phase2="auth=MSCHAPV2"
+}
+
+# eduroam does not work with the new wpa driver
+#sudo systemctl enable wpa_supplicant@.service
+## keep only the wext driver
+#sudo nano /etc/systemd/system/multi-user.target.wants/wpa_supplicant@wlan0.service
 
 
-#
-# network={
-# 	ssid="eduroam"
-# 	scan_ssid=1
-# 	key_mgmt=WPA-EAP
-# 	eap=PEAP
-# 	identity=""
-# 	password=""
-# 	phase2="auth=MSCHAPV2"
-#     }
-#
+#to clone: dd if=/dev/sda   bs=64K conv=noerror,sync status=progress | gzip -c  > ./2019-10-29_pi_clone.img.gz
+# to restore: gunzip -c ./2019-10-29_pi_clone.img.gz | dd of=/dev/sda bs=64K conv=noerror,sync status=progress
+
